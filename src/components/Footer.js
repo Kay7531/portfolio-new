fd0fe4bcc9373ff9b02056057104902966b2df93
User Name <kauth333@gmail.com>
2023-03-09 20:06:43 -0500

    change footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/Kay7531"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -43,7 +43,7 @@ function Footer() {
             </li> */}
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/kautharmumin33/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
